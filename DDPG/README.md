5da60c83666ccf1e57b383d6554fb59a78f99c20
Peter Jochem <peterpennrichjochem@gmail.com>
2020-10-15 16:34:52 -0500

    Fixed the issues preventing me from pushing code

--- DDPG/README.md
+++ DDPG/README.md
@@ -7,6 +7,9 @@ This is an implementation of Deep Deterministic Policy Gradients as described in
 ### Mujoco Environments
 The Mujoco Physics Simulator (Multi-Joint Dynamics with Contact) has a few OpenAI gym environments for simple robots with continuous control. I implemented DDPG with the same hyper parameters as the original DDPG [paper](https://arxiv.org/abs/1509.02971) and applied it to the Hopper-V2 and Cheetah environments. The Hopper and Cheetah environments feature robots who must learn what torques to apply to their motors in order to produce forward translation. <br />
 
+The agent's learned policy is pretty unstable! It has short periods where it hops really well but then some weight updates push it toward pretty poor gaits. The neural networks are pretty shallow right now. Hopefully a larger network would be more robust. Another approach could be to reduce the action space noise or to move away from action space noise and instead use parameter space noise. Below is the agent's cumulatative reward per episode of training. 
+[![](media/hopper_learning2_reward.png)] <br />
+
 Below is a gif of the Hopper's learned policy. A video is available [here](https://youtu.be/E0tvLX5sxv0?t=281) <br />
 [![](media/hopper_learned_policy.gif)] <br />
 
@@ -15,6 +18,7 @@ The agent's learned policy is pretty unstable! It has short periods where it hop
 
 
 Below is a gif of the Cheetah's learned policy. A video is available [here](https://youtu.be/DQCQSEspLhs) <br />
+>>>>>>> 2708bcb2ca668311601fd2ee5e300287f0abbb39
 [![](media/cheetah_learned_policy.gif)]
 
 ### Classic Control Environments
--- DDPG/h3pper/gym-hopping_robot/gym_hopping_robot/envs/hopping_robot_env.py
+++ DDPG/h3pper/gym-hopping_robot/gym_hopping_robot/envs/hopping_robot_env.py
@@ -6,7 +6,8 @@ import time
 import pybullet_data
 import keras
 
-absolute_path_urdf = "/home/peter/Desktop/HoppingRobot_Fall/RL/gym-hopping_robot/gym_hopping_robot/envs/hopping_robot/urdf/hopping_robot.urdf"
+absolute_path_urdf = "/home/peter/Desktop/MSR/Deep_RL/DDPG/h3pper/gym-hopping_robot/gym_hopping_robot/envs/hopping_robot/urdf/hopping_robot.urdf"
+absolute_path_neural_net = "/home/peter/Desktop/MSR/Deep_RL/DDPG/h3pper/gym-hopping_robot/gym_hopping_robot/envs/hopping_robot/neural_nets/my_model"  
 
 class HoppingRobotEnv(gym.Env):       
     
@@ -19,6 +20,8 @@ class HoppingRobotEnv(gym.Env):
         
         self.jointIds=[]
         self.paramIds=[]
+    
+        self.neural_net = keras.models.load_model(absolute_path_neural_net)
 
         p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
         p.setGravity(0, 0, -10)
@@ -32,7 +35,6 @@ class HoppingRobotEnv(gym.Env):
         # Why -10, 10, -10
         self.gravId = p.addUserDebugParameter("gravity", -10, 10, -10) 
         self.homePositionAngles = [0.0, 0.0, 0.0]
-        
         
         activeJoint = 0
         for j in range (p.getNumJoints(self.hopper)):
@@ -49,13 +51,12 @@ class HoppingRobotEnv(gym.Env):
                 self.paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, self.homePositionAngles[activeJoint]))
                 #p.resetJointState(self.hopper, j, self.homePositionAngles[activeJoint])
                 activeJoint = activeJoint + 1
-        
 
         # What exactly does this do?
         p.setRealTimeSimulation(0)
-        
         self.stateId = p.saveState() # Stores state in memory rather than on disk
     
+
     """Reset the robot to the home position"""
     def defineHomePosition(self):
 
@@ -80,6 +81,8 @@ class HoppingRobotEnv(gym.Env):
                 p.resetJointState(self.hopper, j, self.homePositionAngles[activeJoint])
                 activeJoint = activeJoint + 1
 
+    """Update robot's PID controller's control signals. controlSignal is a list of desired joint angles (rads).
+    PyBullet does support direct torque control...iterate in this direction eventually?"""
     def goToHomePosition(self):
 
         activeJoint = 0
@@ -95,16 +98,15 @@ class HoppingRobotEnv(gym.Env):
                 p.resetJointState(self.hopper, j, self.homePositionAngles[activeJoint])
                 activeJoint = activeJoint + 1
 
-    """Check for controller signals. Allows user to use the GUI sliders
-    Do I need this to programatically control robot????"""
-    # controlSignal is the list of 1 joint angle 
+    """Update robot's PID controller's control signals. controlSignal is a list of desired joint angles (rads).
+    PyBullet does support direct torque control...iterate in this direction eventually?"""
     def controller(self, controlSignal):
 
         for i in range(len(self.paramIds)):
             nextJointId = self.paramIds[i]
-            #targetPos = p.readUserDebugParameter(nextJointId) # This reads from the sliders
+            #targetPos = p.readUserDebugParameter(nextJointId) # This reads from the sliders. Useful for debugging        
             targetPos = controlSignal[i] # This uses the control signal parameter
-            p.setJointMotorControl2(self.hopper, self.jointIds[i], p.POSITION_CONTROL, targetPos, force = 100.0)
+            p.setJointMotorControl2(self.hopper, self.jointIds[i], p.POSITION_CONTROL, targetPos, force = 75.0) # 100.0
     
     """Return the robot to its initial state"""
     def reset(self):
@@ -113,14 +115,45 @@ class HoppingRobotEnv(gym.Env):
         self.jointIds=[]
         self.paramIds=[]
         p.removeAllUserParameters() # Must remove and replace
-        self.defineHomePosition()
+        self.goToHomePosition()
         p.restoreState(self.stateId)
-        #self.goToHomePosition()
     
         return self.computeObservation()
 
+    def computeGRF(self, gamma, beta, depth, dx, dy):
+        
+        inVector = [gamma, beta, depth, dx, dy]
+        grf = self.neural_net.predict(inVector)
+        
+        # reformat grf before returning it?
+        return 
+        
+    """ After stepping the simulation, force the robot to be in the y-z plane
+    A bit too hacky? Well, planar physics is well, hacky anyways""" 
+    def planarConstraint(self):
+        
+        robot_position, robot_orientation = p.getBasePositionAndOrientation(self.hopper)
+        x, y, z = robot_position
+        [dx, dy, dz], [wx, wy, wz] = p.getBaseVelocity(self.hopper) 
+
+        """ Force the base to be in the y-z plane. resetBAsePosandOrient sets the
+        velocities to 0, so we must overwrite them with the original velocities. We can translate
+        in the y-z plane and rotate about the x-axis"""
+        p.resetBasePositionAndOrientation(self.hopper, [0.0, y, z], robot_orientation)
+        p.resetBaseVelocity(self.hopper, [0.0, dy, dz], [wx, 0.0, 0.0])
+
     def step(self, action):
         
+        # Forward prop neural network to get GRF, use that to change the gravity
+        # Shoud really compute after every p.stepSimulation
+        # gamma, beta, depth, dx, dy
+        # gamma = velocity through the granular material 
+        # beta = orientation about PyBullet's x axis  
+        #force_x, force_z = self.computeGRF()
+
+        #p.getCameraImage(320, 200)
+        #self.defineHomePosition()
+    
         # Forward prop neural network to get GRF, use that to change the gravity
         # FIX ME
         p.getCameraImage(320, 200)
@@ -128,19 +161,24 @@ class HoppingRobotEnv(gym.Env):
 
         # Step forward some finite number of seconds or milliseconds
         self.controller(action[0])
+        for i in range (3):
+            foot_index = 3     
+            # Must call this each time we stepSimulation
+            # p.applyExternalForce(self.hopper, foot_index, [0, 0, -100.0], [0.0, 0.0, 0.0], p.LINK_FRAME)
+            
+            p.stepSimulation()
+            self.planarConstraint()
         for i in range (10):
                 p.stepSimulation()
 
-                #time.sleep(1.0/240.0)
-                # self.cubePos, self.cubeOrn = p.getBasePositionAndOrientation(self.hopper)
-                #time.sleep(0.001)
-
-    
         # observation = list of joint angles
-
         isOver = self.checkForEnd()
-
         return self.computeObservation(), self.computeReward(isOver), isOver, None
+    
+    def getFootState(self):
+            
+        foot_joint_index = 3 # Known by printing world frame position of links with p.getLinkState(self.hopper, <index#>)
+        foot_position, foot_velocity, foot_reaction_forces, appliedTorque = p.getJointStates(self.hopper, [foot_joint_index])[0]
         # what is info?
         #return observation, reward, done, info
     
@@ -150,7 +188,6 @@ class HoppingRobotEnv(gym.Env):
         foot_position, foot_velocity, foot_reaction_forces, appliedTorque = p.getJointStates(self.hopper, [foot_joint_index])[0]
 
         #print(p.getJointStates(self.hopper, [foot_joint_index]))
-        
         return foot_position, foot_velocity, foot_reaction_forces, appliedTorque
         
 
@@ -165,7 +202,6 @@ class HoppingRobotEnv(gym.Env):
         # Should I give it the x, y, z too?
         return [foot_angle, roll, pitch, yaw]  
 
-
     def checkForEnd(self):
 
         self.robot_position, self.robot_orientation = p.getBasePositionAndOrientation(self.hopper)
@@ -178,39 +214,23 @@ class HoppingRobotEnv(gym.Env):
 
         return False
 
-         
+    """Required for the OpenAI Gym API""" 
     def render(self, mode='human', close = False):
         pass
     
-    """Read the state of the simulation to compute 
-    and return the reward scalar for the agent"""
+    """Read the state of the simulation to compute and return the 
+    reward scalar for the agent. A great video on reward shaping
+    from the legendary control theory youtuber Brian Douglas -> 
+    https://www.mathworks.com/videos/reinforcement-learning-part-4-the-walking-robot-problem-1557482052319.html
+    Remember, you get what you incentivize, not what you want"""
     def computeReward(self, isOver):
 
-        stillAliveBonus = 1.0
+        stillAliveBonus = 0.0625
         
         self.robot_position, self.robot_orientation = p.getBasePositionAndOrientation(self.hopper)
-        x, y, z = self.robot_position
-
-        # Convert quaternion to Euler angles
-        roll, pitch, yaw = p.getEulerFromQuaternion(self.robot_orientation)
-
-        # Reward is inversely proportional to the rotation about the y axis (pitch), and x angle (roll)
-        # Reward is proportional to the forward motion in the +x direction
-        # FIX ME - which axis should we walk on?
-        if (roll == 0.0):
-            roll = 0.001
-        if (pitch == 0.0):
-            pitch = 0.001
+        x, y, z = self.robot_position 
+        [dx, dy, dz], [wx, wy, wz] = p.getBaseVelocity(self.hopper)
         
-        #reward = ((-1.0/roll) + (-1.0/pitch))/1000.0 + (10 * x)
-        reward = -y
-        if (isOver == False):
-            reward = reward + stillAliveBonus
-
-        return reward
-
-            
-
-
-
-
+        # Remember the actions are the joint angles, not the joint torques
+        reward = dy + y  
+         
